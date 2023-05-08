dca30c4668e77f6439ba1784f93ba97e5613d8e1
Mubasshirah <adibaju1994@gmail.com>
2023-05-08 10:56:34 +0600

    H

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 const port=process.env.PORT || 5000;
 const cors=require('cors');
 const categories=require('./data/categories.json');
-
+const news=require('./data/news.json');
 app.use(cors());
 
 app.get('/',(req,res)=>{
@@ -12,6 +12,24 @@ app.get('/',(req,res)=>{
 app.get('/categories',(req,res)=>{
     res.send(categories);
 });
+app.get('/news',(req,res)=>{
+res.send(news);
+});
+app.get('/news/:id',(req,res)=>{
+    const id=req.params.id;
+    const selecteNews=news.find(n=>n._id===id);
+    res.send(selecteNews);
+});
+app.get('/categories/:id',(req,res)=>{
+    const id=parseInt(req.params.id);
+    if(id===0){
+        res.send(news)
+    }
+    else{
+    const selectedNews=news.filter(n=>parseInt(n.category_id)===id);
+    res.send(selectedNews)
+    }
+})
 app.listen(port,()=>{
     console.log(`dragon is running on port ${port}`)
 });
